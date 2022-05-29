5e15e4946a6ade07a2c063ae187fa99c53dc5345
draganradu <draganaurelradu@gmail.com>
2022-05-29 19:42:58 +0200

    Added react form

--- src/pages/CRUD/create.tsx
+++ src/pages/CRUD/create.tsx
@@ -1,3 +1,26 @@
-export const Create:React.FC = () => {
-    return <>Create</>
+import { useForm } from 'react-hook-form';
+
+export const Create: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data:any) => console.log(data);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>First</label><br/>
+            <input {...register('firstName')} /><br/>
+
+            <label>Last</label><br/>
+            <input {...register('lastName', { required: true })} /><br/>
+            {errors.lastName && <p>Last name is required.</p>}
+
+            <label>Age</label><br/>
+            <input {...register('age', { pattern: /\d+/ })} /><br/>
+            {errors.age && <p>Please enter number for age.</p>}
+            <input type="submit" />
+        </form>
+    )
 }
